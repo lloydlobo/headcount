17efe62c5d298890491742eab270224a8851b9bc
Lloyd Lobo <lloydlobo4@gmail.com>
2024-01-23 14:37:13 +0530

    feat: poll active inactive and total entries count

--- main.py
+++ main.py
@@ -44,9 +44,10 @@ def contact_as_row(contact, clazz=""):
     # NOTE: `value` is a string representing the value of the checkbox. This is not displayed on the client-side,
     # but on the server this is the value given to the data submitted with the checkboxes name.
     id_ = contact["id"]  # FIXME: this is visible!!!
+    delete_svg = f"""<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/></svg>"""
     return Markup(f"""
         <tr class="{clazz}">
-            <td>
+            <td scope="row">
                 <label for="ids">
                     <input type='checkbox' name='ids' value='{id_}'>
                 </label>
@@ -55,7 +56,16 @@ def contact_as_row(contact, clazz=""):
             <td>{contact['phone']}</td>
             <td>{contact['email']}</td>
             <td>{'Active' if (contact["status"] == 'Active') else 'Inactive'}</td>
-            <td><button hx-delete="/contact/{id_}">DELETE</button></td>
+            <td>
+                <button 
+                    hx-delete="/contact/{id_}" 
+                    class="contrast"
+                    data-tooltip="Remove {contact['name']}?"
+                    data-placement="left"
+                >
+                    {delete_svg}
+                </button>
+            </td>
         </tr>
     """)
 
@@ -65,15 +75,15 @@ def contacts_to_html_table():
     # <h2 style="--pico-font-size: 1rem; --pico-color: var(--pico-secondary);">{len(contacts)} results</h2>
     return Markup(f"""
         <figure>
-            <table >
+            <table class="!striped">
                 <thead>
                     <tr>
-                        <th></th>
-                        <th>Name</th>
-                        <th>Phone</th>
-                        <th>Email</th>
-                        <th>Status</th>
-                        <th></th>
+                        <th scope="col"></th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Status</th>
+                        <th scope="col"></th>
                     </tr>
                 </thead>
                 <tbody 
@@ -164,6 +174,11 @@ def add_contact():
 
 @app.route("/contact/<contact_id>", methods=["DELETE"])
 def delete_contact(contact_id):
+    """
+    Each row has a button with a hx-delete attribute containing the url on which to issue a DELETE request to delete
+    the row from the server. This request responds with a 200 status code and empty content, indicating that the row
+    should be replaced with nothing.
+    """
     index = None
     for i, contact in enumerate(contacts):
         if str(contact["id"]) == contact_id:
@@ -173,13 +188,51 @@ def delete_contact(contact_id):
     if index is not None:
         contacts.pop(index)
 
-    # html = "".join([contact_as_row(c) for c in contacts])
-    # return Markup(f"{html}")
     return ""
 
 
+@app.route("/count-contacts")
+def count_contacts():
+    return Markup(f"{len(contacts)} total")
+
+
+@app.route("/count-active-contacts")
+def count_active_contacts():
+    counter = 0
+    for c in contacts:
+        if c["status"] == "Active":
+            counter += 1
+    return Markup(f"{counter} active")
+
+
+@app.route("/count-inactive-contacts")
+def count_inactive_contacts():
+    counter = 0
+    for c in contacts:
+        if c["status"] == "Inactive":
+            counter += 1
+    return Markup(f"{counter} inactive")
+
+
+@app.route("/modal", methods=["GET"])
+def get_modal():
+    return Markup(f"""
+        <div id="modal" _="on closeModal add .closing then wait for animationend then remove me">
+            <div class="modal-underlay" _="on click trigger closeModal"></div>
+            <div class="modal-content">
+                <h1>Modal Dialog</h1>
+                This is the modal content.
+                You can put anything here, like text, or a form, or an image.
+                <br>
+                <br>
+                <button _="on click trigger closeModal">Close</button>
+            </div>
+        </div>
+    """)
+
+
 def main():
-    app.run(debug=False)
+    app.run(debug=True)
 
 
 if __name__ == "__main__":
